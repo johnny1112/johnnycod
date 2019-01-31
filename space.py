2ad8bd7ba9f82ffb41ac3ac9e895f4e967a1cc8a
johnny1112 <43337475+johnny1112@users.noreply.github.com>
2019-01-31 14:26:39 +0530

    Create space.py

---
+++ space.py
@@ -0,0 +1,11 @@
+linename = input("Enter line : ")
+k = 0
+ with open(fname, 'r') as f:
+    for line in f:
+        words = line.split()
+        for i in words:
+            for letter in i:
+                if(letter.isspace):
+                    k=k+1
+print(" blank spaces:")
+print(k)
